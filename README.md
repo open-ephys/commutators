8b55140d9f6cb6c544c11d866f7858a6d2900e73
jonnew <jpnewman@mit.edu>
2019-12-22 22:01:38 -0500

    Updates to README to get it ready for public release
    - Added licenses for hardware and software designs
    - Fully updated the BOM on google sheets and put links to in README
    - Update README to match the current firmware operation

--- README.md
+++ README.md
@@ -1,37 +1,52 @@
-# Commutator for serialized, 6 DOF-tracking headstages and miniscopes
+# Coaxial commutator for serialized headstages and miniscopes
 The wide-spread availability of 6 degree of freedom pose tracking using
 internal-measurement units (IMUs) allows continuous monitoring of an animal's
 rotational state in an environment. This obviates the need for tether torque
-measurements to drive an active commutator since the drift-free rotational
-state of the animal is known in real-time, and the commutator can simply
-follow along. This permits the use of extremely thin [coaxial
+measurements to drive an active commutator since the rotational state of the
+animal is known in real-time, and the commutator can simply follow along. This
+permits the use of extremely thin [coaxial
 tethers](http://www.axon-cable.com/en/02_products/06_coaxial-cables/02/index.aspx)
 that cannot function with a standard active commutator because they are too
 flexible to translate rotational torque.
 
+![Zero-torque coaxial commutator.](./resources/demo.gif)
+
 This commutator functions with coaxially-serialized head-borne recording
 systems that provide appropriate rotational tracking information. Currently
 supported devices are next-generation, serialized headstages and miniscopes
-such as [headstage-64](), [headstage-256](), and UCLA Miniscope Rev. 4.0.
-However, this device operations competely independently from the headstage
+such as [headstage-64](../headstage-64/README.md),
+[headstage-256](../headstage-256/README.md), and UCLA Miniscope Rev. 4.0f.
+However, this device operations completely independently from the head stage
 itself, so can be used with any device that provides accurate, drift-free
-rotational state.
+rotational state. It can even be used without an IMU, e.g. using video-based
+rotation tracking since its remote control interface is agnostic to how
+rotational measurements are taken.
+
+## Features
+- High bandwidth link up to 18 GHz
+- Optical table & 80/20 rail mountable
+- Remote control using JSON-encoded commands
+- Manual control 
+- Indication LED
+    - Can be completely turned off
+- Advanced stepper driver (TMC2130)
+    - Voltage-controlled for silent operation 
+    - Precise motion using step interpolation (256 uSteps/step)
+- USB powered and controlled
+    - Internal super-capacitor circuitry prevents loading the USB bus during
+      motion
 
 ## Usage
 
 ### LED
-The LED tells you about the commutator state
+The LED tells you about the commutator state:
 
-1. Flashing red: commutator is charging internal super-capacitors.  All
-   controls and motor operation are locked. Wait until this process 
-   completes to use the device.
-1. Solid red: commutator is disabled. Motor is turned off and will not
+1. Flashing red (*Charging*): commutator is charging internal super-capacitors.
+   All controls and motor operation are locked. Wait until this process
+   completes to use the device. It can take up to 30 seconds.
+1. Solid red (*Disabled*): commutator is disabled. Motor is turned off and will not
    turn or respond to button presses or external commands.
-1. Pink: commutator is enabled permits manual control only. The device will
-   not respond to remote turn commands.
-1. Green: commutator is enabled and permits remote turn control. Turn
-   buttons are inactive. Stop/Go and LED buttons remain active.
-1. Blue: commutator is enabled and permits both remote (RPC) and manual
+1. Green (*Enabled*): commutator is enabled and permits both remote and manual
    (button) turn control. Buttons take precedence over remote commands.
 
 The LED can be turned off by pressing and holding it > 500 msec. It can be
@@ -43,16 +58,14 @@ The front panel has four buttons.
 - __Stop/Go__: toggle commutator enable/disable.
     - *Disabled* (LED is red): All motor output will halt instantly, and motor
       driver is powered down. Pressing directional buttons in the stopped state
-      will not work.  All target turns provided via RPC will be cleared, such
-      that re-enable the motor will not result in the commutator re-engaging an
-      old target position. In this state, pressing and holding the Stop/Go
-      button for > 0.5 second, or sending the approriate RPC will enable the
-      device.
-    - *Enabled* (LED is blue, green, or pink): When in the *enabled* state, the
-      LED will be blue, green, or pink and the motor can be turned via button
-      presses or RPCs depending on the operation mode (see Mode Button). In
-      this state, pressing the Stop/Go button will instantly disable the
-      device.
+      will not work. All target turns provided via remote calls will be
+      cleared, such that re-enable the motor will not result in the commutator
+      re-engaging an old target position. In this state, pressing and holding
+      the Stop/Go button for > 0.5 second, or sending the approriate remote
+      command will enable the device.
+    - *Enabled* (LED  green): When in the *enabled* state, the LED will be
+      green and the motor can be turned via button presses or RPCs . In this
+      state, pressing the Stop/Go button will instantly disable the device.
 
 - __Directional__ (2x): Manually control the motor rotation in the
   direction indicated on each button when the commutator is *Enabled* and in
@@ -61,14 +74,14 @@ The front panel has four buttons.
   such that releasing them will not result in the commutator re-engaging an old
   target position.
 
-- __Mode__: pressing the LED toggles the operation mode.
-  - *Manual* (LED is pink): commutator is enabled and responds to manual,
-    button-based turn control only
-  - *Remote* (LED is green): commutator is enabled and responds to remote,
-    RPC-based turn control only
-  - *Remote/Manual* (LED is blue): commutator is enabled and responds to both
-    remote and manual turn control. Manual control takes presidence over remote
-    and will clear all remote targeting state.
+- __LED__: pessing and holding the the LED turns it off. Pressing it again turns it back on.
+    - *Manual* (LED is pink): commutator is enabled and responds to manual,
+      button-based turn control only
+    - *Remote* (LED is green): commutator is enabled and responds to remote,
+      RPC-based turn control only
+    - *Remote/Manual* (LED is blue): commutator is enabled and responds to both
+      remote and manual turn control. Manual control takes presidence over
+      remote and will clear all remote targeting state.
 
 ### Remote control interface
 When in *Remote* or *Remote/Manual* mode, the commutator accepts JSON-encoded
@@ -103,27 +116,28 @@ interface, are saved in non-volatile memory each time they are changed. The
 device will start in the same state it was last used.
 
 ## Firmware
-The controller firmware is located [here](./firmware/). It runs on a
+The controller firmware is located [here](./firmware/commutator). It runs on a
 [Teensy 3.2](https://www.pjrc.com/store/teensy32.html). To compile 
 this firmware and program the microcontroller, you need the following 
 dependencies:
 
 - [Arduino IDE](https://www.arduino.cc/en/Main/Software)
 - [Teensyduino add-on](https://www.pjrc.com/teensy/td_download.html)
-- [TeensyStep](https://github.com/luni64/TeensyStep)
+- [AccelStepper](https://www.airspayce.com/mikem/arduino/AccelStepper/)
 - [Arduino JSON](https://arduinojson.org/)
 
 The firmware can be uploaded to the device using the [Arduino
 IDE](https://www.arduino.cc/en/Main/Software). _Note that you will need to add
 the [Teensyduino add-on](https://www.pjrc.com/teensy/teensyduino.html) to to
-the Arduino IDE to program the Teensy_.When installing Teensyduino, you should
-opt to install all of the bundled libraries as well. This takes care of installing
-`i2c_t3.h` and `SPI.h` rather than having to install them manually.
+the Arduino IDE to program the Teensy_. When installing Teensyduino, you should
+opt to install all of the bundled libraries as well. This takes care of
+installing `AccelStepper` library rather than having to install it manually.
+ArduinoJSON can be installed through the Arduino IDE's package manager.
 
 ## Construction
 
 ### Bill of Materials
-The BOM is located [here](https://docs.google.com/spreadsheets/d/1M2R0Q2-OuRHzctt05BxtA3hxNcCHtRZHORzCKElmG1Q/edit?usp=sharing).
+The BOM is located [here](https://docs.google.com/spreadsheets/d/1F-KWcdvH_63iXjZf0cgCfDiFX6XXW3qw6rlR8DZrFpQ/edit#gid=219603647).
 
 ### Mechanical
 The mechanical component of the commutator are as follows:
@@ -134,11 +148,9 @@ The mechanical component of the commutator are as follows:
 1. Housing (3D-printed)
 1. Some fasterners
 
-Mechanical designs are located [here](./mechanical/). STL files 
-for 3D printing are located in the `stl` subdirectory.
-
-Links to purchase each of these components, including 3D-printed parts, can be 
-found on the BOM.
+Mechanical designs are located [here](./mechanical/). STL files for 3D printing
+are located in the `stl` subdirectory. Links to purchase each of these
+components, including 3D-printed parts, can be found on the BOM.
 
 ### Electronics
 The board used to control the commutator consists of the following elements:
@@ -188,3 +200,24 @@ OHL v.1.2. (http://ohwr.org/cernohl). This documentation is distributed WITHOUT
 ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING OF MERCHANTABILITY, SATISFACTORY
 QUALITY AND FITNESS FOR A PARTICULAR PURPOSE. Please see the CERN OHL v.1.2 for
 applicable conditions
+
+## Software/Firmware License
+Copyright 2019 Jonathan P. Newman 2019
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ firmware/commutator/commutator.ino
@@ -0,0 +1,635 @@
+#include <AccelStepper.h>
+#include <ArduinoJson.h>
+#include <EEPROM.h>
+#include <SPI.h>
+#include <i2c_t3.h>
+#include <math.h>
+
+// Button read parameters
+#define HOLD_MSEC           300
+#define TOUCH_MSEC          5
+
+// Stepper parameters
+#define DETENTS             200
+#define USTEPS_PER_STEP     16
+#define USTEPS_PER_REV      (DETENTS * USTEPS_PER_STEP)
+
+// Capacitive touch buttons
+#define CAP_TURN_CW         0
+#define CAP_MODE_SEL        1
+#define CAP_TURN_CCW        15
+#define CAP_STOP_GO         17
+#define CAP_DELTA           30 // delta capacitance required
+
+// Stepper driver pins
+#define MOT_DIR             14
+#define MOT_STEP            16
+#define MOT_CFG0_MISO       12
+#define MOT_CFG1_MOSI       11
+#define MOT_CFG2_SCLK       13
+#define MOT_CFG3_CS         10
+#define MOT_CFG6_EN         9
+
+// Power options
+#define MOT_POW_EN          22
+#define VMID_SEL            20
+#define CHARGE_CURR         21
+#define nPOW_FAIL           23
+#define CHARGE_CURR_THRESH  20 // Charge level required to allow operation. Arbitrary units
+
+// I2C
+#define SDA                 18
+#define SCL                 19
+
+// RGB Driver
+#define IS31_ADDR           0x68
+#define IS31_SHDN           5
+#define IS31_BM             2
+
+// TMC2130 registers
+#define WRITE_FLAG          (1<<7) //write flag
+#define READ_FLAG           (0<<7) //read flag
+#define REG_GCONF           0x00
+#define REG_GSTAT           0x01
+#define REG_IHOLD_IRUN      0x10
+#define REG_CHOPCONF        0x6C
+#define REG_COOLCONF        0x6D
+#define REG_DCCTRL          0x6E
+#define REG_DRVSTATUS       0x6F
+
+// Settings address start byte
+#define SETTINGS_ADDR_START 0
+
+// Controller state
+struct Context {
+    bool led_on = true;
+    bool commutator_en = false;
+    float speed_rpm = 100;
+    float accel_rpmm = 400;
+};
+
+// Holds the current state
+Context ctx;
+
+// Save settings flag
+bool save_required = false;
+
+// Stepper motor
+AccelStepper motor(AccelStepper::DRIVER, MOT_STEP, MOT_DIR);
+double target_turns = 0;
+
+// Global timer
+elapsedMillis global_millis;
+
+// Motor update timer
+volatile int motor_settled_cnt = 0;
+IntervalTimer mot_timer;
+
+// Triggered in case of under current from host
+// TODO: use somehow
+volatile bool power_failure = false;
+
+// I was touched..., by the power..., on my unit..., in broooooooooooad
+// daylight
+enum TouchState { untouched, touched, held };
+
+struct TouchSensor {
+
+    // Given last_state and current check, what is the consecutive for the state
+    // of this touch sensor?
+    TouchState result = untouched;
+
+    int pin = 0;
+    int calib_val = 0;
+    bool fresh = true;
+};
+
+// Touch Sensors
+// NB: = {.pin = whatever} -> can't, compiler does not implement
+TouchSensor touch_cw;
+TouchSensor touch_ccw;
+TouchSensor touch_mode;
+TouchSensor touch_stopgo;
+
+void calibrate_touch(TouchSensor *sensor, unsigned int msec)
+{
+    global_millis = 0;
+
+    int k = 0;
+    long long val = 0;
+    while (global_millis < msec) {
+
+        val += touchRead(sensor->pin);
+        k++;
+    }
+
+    sensor->calib_val = (int)(val / k);
+}
+
+void check_touch(TouchSensor *sensor, unsigned int hold_msec = HOLD_MSEC)
+{
+    global_millis = 0;
+    bool fingy = true;
+    int consecutive = 0;
+    int k = 0;
+    sensor->fresh = false;
+
+    // Check the pad for a min of TOUCH_MSEC and a max of HOLD_MSEC to get
+    // consensus on fingy presence
+    while ((fingy && global_millis <= hold_msec)
+           || global_millis < TOUCH_MSEC) {
+        fingy = ((touchRead(sensor->pin) - sensor->calib_val) > CAP_DELTA);
+        consecutive += fingy ? 1 : 0;
+        k++;
+    }
+
+    // Do we deserve an update?
+    auto consensus = consecutive > k * 0.9;
+
+    if (global_millis >= hold_msec && consensus) {
+        sensor->fresh = sensor->result != held;
+        sensor->result = held;
+        return;
+    }
+
+    // Transitions out of held are forbidden to debounce
+    if (consensus && sensor->result != held) {
+        sensor->fresh = sensor->result != touched;
+        sensor->result = touched;
+        return;
+    }
+
+    // Do we deserve an update?
+    consensus = consecutive < k * 0.1;
+
+    if (consensus) {
+        sensor->fresh = sensor->result != untouched;
+        sensor->result = untouched;
+        return;
+    }
+}
+
+uint8_t tmc_write(uint8_t cmd, uint32_t data)
+{
+    uint8_t s;
+
+    digitalWriteFast(MOT_CFG3_CS, LOW);
+
+    s = SPI.transfer(cmd);
+    SPI.transfer((data >> 24UL) & 0xFF) & 0xFF;
+    SPI.transfer((data >> 16UL) & 0xFF) & 0xFF;
+    SPI.transfer((data >> 8UL) & 0xFF) & 0xFF;
+    SPI.transfer((data >> 0UL) & 0xFF) & 0xFF;
+
+    digitalWriteFast(MOT_CFG3_CS, HIGH);
+
+    return s;
+}
+
+uint8_t tmc_read(uint8_t cmd, uint32_t *data)
+{
+    uint8_t s;
+
+    tmc_write(cmd, 0UL); // set read address
+
+    digitalWriteFast(MOT_CFG3_CS, LOW);
+
+    s = SPI.transfer(cmd);
+    *data = SPI.transfer(0x00) & 0xFF;
+    *data <<= 8;
+    *data |= SPI.transfer(0x00) & 0xFF;
+    *data <<= 8;
+    *data |= SPI.transfer(0x00) & 0xFF;
+    *data <<= 8;
+    *data |= SPI.transfer(0x00) & 0xFF;
+
+    digitalWriteFast(MOT_CFG3_CS, HIGH);
+
+    return s;
+}
+
+// Setting state load/save
+void load_settings()
+{
+    int addr = SETTINGS_ADDR_START;
+
+    byte good_settings = 0x0;
+    EEPROM.get(addr, good_settings); // check good settings flag
+    if (good_settings != 0x12)
+        return;
+
+    EEPROM.get(addr += sizeof(Context), ctx);
+}
+
+void save_settings()
+{
+    int addr = SETTINGS_ADDR_START;
+    EEPROM.put(addr, 0x12); // good settings flag
+    EEPROM.put(addr += sizeof(Context), ctx);
+}
+
+// Motor target update. We integrate turns in the target position and apply to
+// motor's motion.
+void turn_motor(double turns)
+{
+    // Relative move
+    target_turns += turns;
+    motor.moveTo(lround(target_turns * (double)USTEPS_PER_REV));
+}
+
+// Emergency motor stop/reset
+void hard_stop() {
+    motor.setAcceleration(1e6);
+    motor.stop();
+    motor.runToPosition();
+    motor.setCurrentPosition(0);
+    target_turns = 0.0;
+    update_motor_accel();
+}
+
+void set_rgb_color(byte r, byte g, byte b)
+{
+    // Set PWM state
+    Wire.beginTransmission(IS31_ADDR);
+    Wire.write(0x04);
+    Wire.write(r);
+    Wire.write(g);
+    Wire.write(b);
+    Wire.endTransmission();
+
+    // Update PWM
+    Wire.beginTransmission(IS31_ADDR);
+    Wire.write(0x07);
+    Wire.write(0x00);
+    Wire.endTransmission();
+}
+
+void update_rgb()
+{
+    if (!ctx.led_on) {
+        set_rgb_color(0x00, 0x00, 0x00);
+        return;
+    }
+
+    if (!ctx.commutator_en) {
+        set_rgb_color(255, 0, 0);
+        return;
+    }
+
+    set_rgb_color(1, 20, 7);
+}
+
+void setup_cap_touch()
+{
+    touch_cw.pin = CAP_TURN_CW;
+    touch_ccw.pin = CAP_TURN_CCW;
+    touch_mode.pin = CAP_MODE_SEL;
+    touch_stopgo.pin = CAP_STOP_GO;
+
+    calibrate_touch(&touch_cw, 10);
+    calibrate_touch(&touch_ccw, 10);
+    calibrate_touch(&touch_mode, 10);
+    calibrate_touch(&touch_stopgo, 10);
+}
+
+void setup_rgb()
+{
+    // I2C for RGB LED
+    Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, 400000);
+    Wire.setDefaultTimeout(200000); // 200ms
+
+    // Enable LED current driver
+    pinMode(IS31_SHDN, OUTPUT);
+    digitalWriteFast(IS31_SHDN, HIGH);
+    delay(1);
+
+    // Set max LED current
+    Wire.beginTransmission(IS31_ADDR);
+    Wire.write(0x03);
+    Wire.write(0x08); // Set max current to 5 mA
+    Wire.endTransmission();
+
+    // Default color
+    update_rgb();
+
+    // Enable current driver
+    Wire.beginTransmission(IS31_ADDR);
+    Wire.write(0x00);
+    Wire.write(0x20); // Enable current driver
+    Wire.endTransmission();
+}
+
+void setup_motor()
+{
+    // set pins
+    pinMode(MOT_CFG6_EN, OUTPUT);
+    digitalWriteFast(MOT_CFG6_EN, HIGH); // Inactivate driver (LOW active)
+    pinMode(MOT_DIR, OUTPUT);
+    digitalWriteFast(MOT_DIR, LOW); // LOW or HIGH
+    pinMode(MOT_STEP, OUTPUT);
+    digitalWriteFast(MOT_STEP, LOW);
+
+    pinMode(MOT_CFG3_CS, OUTPUT);
+    digitalWriteFast(MOT_CFG3_CS, HIGH);
+    pinMode(MOT_CFG1_MOSI, OUTPUT);
+    digitalWriteFast(MOT_CFG1_MOSI, LOW);
+    pinMode(MOT_CFG0_MISO, INPUT);
+    digitalWriteFast(MOT_CFG0_MISO, HIGH);
+    pinMode(MOT_CFG2_SCLK, OUTPUT);
+    digitalWriteFast(MOT_CFG2_SCLK, LOW);
+
+    // init SPI
+    SPI.begin();
+    SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
+
+    // TMC2130 config
+    // voltage on AIN is current reference
+    // Stealthchop is on
+    tmc_write(WRITE_FLAG | REG_GCONF, 0x00000003UL);
+
+    // IHOLD=0x10, IRUN=0x10
+    tmc_write(WRITE_FLAG | REG_IHOLD_IRUN, 0x00001010UL);
+
+    switch (USTEPS_PER_STEP) {
+        case 1:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x08008008UL); //  1 microsteps,
+            break;
+        case 2:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x07008008UL); //  2 microsteps,
+            break;
+        case 4:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x06008008UL); //  4 microsteps,
+            break;
+        case 8:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x05008008UL); //  8 microsteps,
+            break;
+        case 16:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x04008008UL); // 16 microsteps,
+            break;
+        case 32:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x03008008UL); // 32 microsteps,
+            break;
+        case 64:
+            tmc_write(WRITE_FLAG | REG_CHOPCONF, 0x02008008UL); // 64 microsteps,
+            break;
+        case 128:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x01008008UL); //128 microsteps,
+            break;
+        default:
+            tmc_write(WRITE_FLAG|REG_CHOPCONF,   0x08008008ul); //  1 microsteps,
+            break;
+    }
+
+    // Setup motor driver
+    update_motor_speed();
+    update_motor_accel();
+
+    // Minimum pulse width
+    motor.setMinPulseWidth(3);
+
+    // Setup run() timer
+    mot_timer.begin(run_motor_isr, 10);
+}
+
+void run_motor_isr()
+{
+    if (motor.distanceToGo() != 0) {
+        //enable motor
+        digitalWriteFast(MOT_CFG6_EN, LOW);
+        motor.run();
+        motor_settled_cnt = 0;
+    } else {
+        motor_settled_cnt++;
+        target_turns = 0.0; // Take opportunity to reset motor position to 0
+        motor.setCurrentPosition(0);
+    }
+
+    if (motor_settled_cnt == 50e3)
+        digitalWriteFast(MOT_CFG6_EN, HIGH);
+}
+
+void power_fail_isr()
+{
+    power_failure = true;
+}
+
+void setup_power()
+{
+    // Turn on breathing mode
+    set_rgb_color(255, 0, 0);
+    Wire.beginTransmission(IS31_ADDR);
+    Wire.write(0x02);
+    Wire.write(0x20); // Turn on breathing mode
+    Wire.endTransmission();
+
+    // Turn on the motor power
+    pinMode(MOT_POW_EN, OUTPUT);
+    digitalWriteFast(MOT_POW_EN, HIGH);
+    delay(100);
+
+    pinMode(VMID_SEL, OUTPUT);
+    digitalWriteFast(VMID_SEL, HIGH); // 2.7V across each super cap
+
+    pinMode(nPOW_FAIL, INPUT);
+    attachInterrupt(nPOW_FAIL, power_fail_isr, FALLING);
+
+    // Wait for charge current stabilize and breath LED in meantime
+    while (analogRead(CHARGE_CURR) >= CHARGE_CURR_THRESH)
+        delay(10);
+
+    Wire.beginTransmission(IS31_ADDR);
+    Wire.write(0x02);
+    Wire.write(0x00); // Turn off breathing mode
+    Wire.endTransmission();
+}
+
+void update_motor_speed()
+{
+    // * 2 is because this is a 2x reduction gear
+    auto max_speed = (float)USTEPS_PER_REV * 2 * ctx.speed_rpm / 60.0;
+    motor.setMaxSpeed(max_speed);
+}
+
+void update_motor_accel()
+{
+    // * 2 is because this is a 2x reduction gear
+    auto a = (float)USTEPS_PER_REV * 2 * ctx.accel_rpmm / 60.0;
+    motor.setAcceleration(a);
+}
+
+void poll_led()
+{
+    // Poll the mode button
+    check_touch(&touch_mode, 100);
+
+    // NB: see note at declaration
+
+    if (touch_mode.result == held && touch_mode.fresh && ctx.led_on) {
+        // If held and fresh, toggle LED
+        ctx.led_on = false;
+        save_required = true;
+
+    } else if (touch_mode.result == held && touch_mode.fresh && !ctx.led_on) {
+        // If LED is off and touched and fresh, toggle LED
+        ctx.led_on = true;
+        save_required = true;
+    }
+}
+
+void poll_turns()
+{
+    // If the commutator is not enabled, or under pure remote control, then
+    // these buttons can't do anything
+    if (!ctx.commutator_en)
+        return;
+
+    // Poll the cw button
+    check_touch(&touch_cw, 100);
+    if (touch_cw.result == held) {
+
+        // If the motor is turning, stop it
+        hard_stop();
+
+        // Set huge target
+        motor.move(10e6);
+
+        while (touch_cw.result == held)
+            check_touch(&touch_cw);
+
+        // Set all targets to 0 because we are overriding
+        // and Disable driver
+        hard_stop    // NB: see note at declaration
+
+();
+
+        return;
+    }
+
+    // Poll the ccw button
+    check_touch(&touch_ccw, 100);
+    if (touch_ccw.result == held) {
+
+        // If the motor is turning, stop it
+        hard_stop();
+
+        // Set huge targetr
+        motor.move(-10e6);
+
+        while (touch_ccw.result == held)
+            check_touch(&touch_ccw);
+
+        // Set all targets to 0 because we are overriding
+        // and Disable driver
+        hard_stop();
+
+        return;
+    }
+}
+
+void poll_stop_go()
+{
+    check_touch(&touch_stopgo, 100);
+
+    if (touch_stopgo.result && touch_stopgo.fresh
+        && ctx.commutator_en) { // Touch or hold can turn off the motor
+        ctx.commutator_en = false;
+        save_required = true;
+
+        // Hard disable/reset on motor
+        hard_stop();
+
+    } else if (touch_stopgo.result == held && touch_stopgo.fresh
+        && !ctx.commutator_en) { // Long hold required turn on the motor after disable
+
+        ctx.commutator_en = true;
+        save_required = true;
+    }
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    // Load parameters from last use
+    load_settings();
+
+    // Setup each block of the board
+    setup_rgb(); // Must come first, used by all that follow
+    setup_power();
+    setup_cap_touch();
+    setup_motor();
+}
+
+void loop()
+{
+    // Poll the stop/go button and update on change
+    poll_stop_go();
+
+    // Poll the mode button and update on change
+    poll_led();
+
+    // Poll manual turn buttons
+    poll_turns();
+
+    // Memory pool for JSON object tree
+    if (Serial.available()) {
+
+        StaticJsonBuffer<200> buff;
+        JsonObject &root = buff.parseObject(Serial);
+
+        if (root.success()) {
+
+            if (root.containsKey("enable")) {
+                ctx.commutator_en = root["enable"].as<bool>();
+                if (!ctx.commutator_en)
+                    hard_stop();
+                save_required = true;
+            }
+
+            if (root.containsKey("speed")) {
+
+                auto rpm = root["speed"].as<float>();
+
+                // Bound speed
+                if (rpm > 0 && rpm <= 1000)
+                    ctx.speed_rpm = rpm;
+                else if (rpm > 1000)
+                    ctx.speed_rpm = 1000;
+
+                update_motor_speed();
+                save_required = true;
+            }
+
+            if (root.containsKey("accel")) {
+
+                auto rpmm = root["accel"].as<float>();
+
+                // Bound speed
+                if (rpmm > 0 && rpmm <= 1000)
+                    ctx.accel_rpmm = rpmm;
+                else if (rpmm > 1000)
+                    ctx.accel_rpmm = 1000;
+
+                update_motor_accel();
+                save_required = true;
+            }
+
+            if (root.containsKey("led")) {
+                ctx.led_on = root["led"].as<bool>();
+                save_required = true;
+            }
+
+            if (root.containsKey("turn") && ctx.commutator_en) {
+                turn_motor(2 * root["turn"].as<double>()); // 2 * for reduction gear
+            }
+        }
+    }
+
+    // Update rgb
+    update_rgb();
+
+    if (save_required) {
+       save_settings();
+       save_required = false;
+    }
+}
